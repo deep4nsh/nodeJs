fd39359cf385c88d7266a0de18d492db85f95958
deepansh <deepansh.gup124@gmail.com>
2026-02-09 12:05:48 +0530

    feat: Create `test2.txt` with content and append text to `test.txt` in `index.js`.

--- multer-image-upload/index.js
+++ multer-image-upload/index.js
@@ -1,2 +1,5 @@
 const fs = require('fs');
-fs.writeFileSync('test.txt', 'Hello World'); 
+fs.writeFileSync('test.txt', 'Hello World');
+fs.appendFileSync('test.txt', 'and I am happy.!');
+
+fs.cpSync('test.txt', 'test2.txt');
