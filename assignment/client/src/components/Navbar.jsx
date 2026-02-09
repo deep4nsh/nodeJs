bdb21967d885efa6eb8230b4ed7db4cb0f5b0339
deepansh <deepansh.gup124@gmail.com>
2026-02-09 12:30:00 +0530

    feat: Replace Navbar text logo with an image, update server message, and refresh package-lock.json.

--- assignment/client/src/components/Navbar.jsx
+++ assignment/client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
     return (
         <nav>
-            <div>MyLogo</div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSZVBdSn-1xDcTSccAw3D6ISpSIP07nzFcQA&s" alt="" />
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/main">Main</Link></li>
