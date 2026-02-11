0fe88111dd869c96b577a95b9a0cf79b3d9ab1b4
deepansh <deepansh.gup124@gmail.com>
2026-02-11 09:51:01 +0530

    Remove old file handling, add OS module utilities, and introduce new JSON data file.

--- work/index.js
+++ work/index.js
@@ -1,7 +1,27 @@
-const fs = require('fs');
-fs.writeFileSync('test.txt', 'Hello World');
-fs.appendFileSync('test.txt', 'and I am happy.!');
+// const fs = require('fs').promises; 
+// async function readJsonFile(filePath) {
+//     try {
+//         const data = await fs.readFile('myfile.txt', 'utf8');
+//         const jsonObject = JSON.parse(data);
+//         console.log('Read data:', jsonObject);
+//         return jsonObject;
+//     } catch (error) {
+//         console.error('Error reading or parsing file:', error);
+//         throw error;
+//     }
+// }
 
-fs.cpSync('test.txt', 'test1.txt');
+// readJsonFile('myfile.txt');
 
-fs.unlinkSync('test1.txt');
+
+const os = require('os');
+console.log('platform', os.platform());
+console.log('architecture', os.arch());
+console.log('total memory', os.totalmem());
+console.log('free memory', os.freemem());
+console.log('uptime', os.uptime());
+console.log('homedir', os.homedir());
+console.log('hostname', os.hostname());
+console.log('network interfaces', os.networkInterfaces());
+//userinfo
+console.log('userinfo', os.userInfo());
